2d2a985049e5d2a3ef4806d86e850751d4c5b4d4
Eduardo <eduarduhh@live.com>
2021-04-18 18:06:43 -0300

    Subindo aplicacao da aula

---
+++ src/main/java/br/com/alura/forum/config/validacao/ErroDTO.java
@@ -0,0 +1,27 @@
+package br.com.alura.forum.config.validacao;
+
+public class ErroDTO {
+    private String field;
+    private String message;
+
+    public ErroDTO(String field, String message) {
+        this.field = field;
+        this.message = message;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
